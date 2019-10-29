2061e26a3adf354e0cefd300735de652cf27dfbe
Gursharanpreet Singh <gp3.singh@gmail.com>
2019-10-29 19:13:18 +0530

    add another test

--- trading_engine/run_tests.py
+++ trading_engine/run_tests.py
@@ -1,7 +1,7 @@
 import importlib
 import sys
 from config_manager import config_manager
-from tests import test_data_engine
+from tests import test_data_engine, test_portfolio_manager
 
 class run_tests:
 
@@ -10,6 +10,7 @@ class run_tests:
 
 	def run( self ):
 		test_data_engine.test_data_engine( self.config ).run()
+		test_portfolio_manager.test_portfolio_manager( self.config ).run()
 
 if __name__ == '__main__':
 	test_runner = run_tests()
