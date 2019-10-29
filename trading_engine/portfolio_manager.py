e48946749fd74f144f502e4454e291b46d678db8
Gursharanpreet Singh <gp3.singh@gmail.com>
2019-10-29 19:13:53 +0530

    update portfolio manager

--- trading_engine/portfolio_manager.py
+++ trading_engine/portfolio_manager.py
@@ -1,19 +1,31 @@
+from config_manager import config_manager
 
 class portfolio_manager:
 
 	def __init__( self, config ):
-		portfolio_file_name = config.get('portfolio.file')
-		self.portfolio = config_reader.read_config( portfolio_file_name )
+		self.portfolio_file_name = config.get('portfolio.file')
+		self.portfolio = config_manager.read_config( self.portfolio_file_name )
 
 	def buy( self, symbol, quantity ):
+		assert( quantity > 0 );
 		# we bought, so add the quantity in our portfolio
 		self.portfolio[ symbol ] = self.portfolio[ symbol ] + quantity;
 
 	def sell( self, symbol, quantity ):
+		assert( quantity > 0 );
 		# we sold, so we remove the quantity in our portfolio
 		self.portfolio[ symbol ] = self.portfolio[ symbol ] - quantity;
 
-	def __del__( self ):
-		portfolio_file_name = config.get('portfolio.file')
-		config_manager.write_config( portfolio_file_name, self.portfolio );
-		
+	def reverse_position( self, symbol ):
+		existing_pos = self.portfolio[symbol]
+		self.portfolio[symbol] = - existing_pos;
+		return existing_pos;
+
+	def get_position( self, symbol ):
+		return self.portfolio[symbol];
+
+	def set_position( self, symbol, position ):
+		self.portfolio[symbol] = position;
+
+	def dump_portfolio( self ):
+		config_manager.write_config( self.portfolio_file_name, self.portfolio );
